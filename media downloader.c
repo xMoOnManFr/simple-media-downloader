25cd3cc06933f31292b21979a68a0f2734a7a1d7
xMoOnManFr <149280282+xMoOnManFr@users.noreply.github.com>
2026-08-29 07:17:56 +0200

    GIF downloading actually works + timestamp isn't shit logic

--- media downloader.c
+++ media downloader.c
@@ -5,7 +5,7 @@
 
 int main(void){
     //Startup text
-    printf("Welcome to the Media Downloader!\n");
+    printf("Welcome to the Media Downloader!\n\n");
 
     char url[256];
 
@@ -29,53 +29,68 @@ int main(void){
     int format; 
     scanf("%d", &format);
 
-    char *rm = "del temp_vid.mp4";
-    char yt_dlp[128];
+    //Vars needed for downloading and encoding magic
+    char yt_dlp[192];
     char ffmpeg[256];
     char *temp_name = "temp_vid";
 
+    //Logic to get different file names, also serves as a timestamp to sort files
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
     char buffer[80];
+
+    //TODO: Write the file to the selected folder (If not bored create the folder/Verify it exist but will need extra imports)
+    char *folder_path = "C:\\Users\\moonman\\Documents\\cat gifs";
     
     // %d = day, %m = month, %y = 2-digit year
-    strftime(buffer, sizeof(buffer), "%d_%m_%y", t);
+    strftime(buffer, sizeof(buffer), "%d_%m_%y_%S_%M_%H",t);
+
+    
 
 
+    //Logic to download as an MP4 
     if(format == 1){
         printf("You selected to download in .MP4\n");
         printf("The download will now start\n");
 
         //Download the video
-        snprintf(yt_dlp,sizeof(yt_dlp), "yt-dlp \"%s\" -o \"video_%s\"", url,buffer);
+        //FIX: the time method only allows for 1 download of each type per day
+        snprintf(yt_dlp, sizeof(yt_dlp), "yt-dlp \"%s\" -o \"%s\\video_%s.mp4\"", url, folder_path, buffer);
+        //snprintf(yt_dlp,sizeof(yt_dlp), "yt-dlp \"%s\" -o \"video_%s\%s\"", url,buffer);
         system(yt_dlp);
 
         //TODO: Open yt-dlp and download file, then write the file to the selected folder
     
+    //Logic to download as an mp4 and reencode in GIF with ffmpeg
     }else if(format == 2){
-        printf("You selected to download in .GIF\n");
-        printf("The download will now start\n");
+    printf("You selected to download in .GIF\n");
+    printf("The download will now start\n");
 
-        //Download the video
-        snprintf(yt_dlp,sizeof(yt_dlp), "yt-dlp \"%s\" -o temp_vid.mp4", url);
-        system(yt_dlp);
+    // Download the video (use temp_name here, not video_<date>)
+    snprintf(yt_dlp, sizeof(yt_dlp), "yt-dlp \"%s\" -o \"%s\\%s.mp4\"", url, folder_path, temp_name);
+    system(yt_dlp);
 
-        snprintf(ffmpeg, sizeof(ffmpeg), "ffmpeg -i \"%s.mp4\" -vf \"fps=20,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" \"gif_%s.gif\"", temp_name, buffer);
-        system(ffmpeg);
-        printf("Download complete\n");
-        printf("Deleting temporary file\n");
-        system(rm);
-        printf("Temporary file deleted, Exiting program");
-    
-    }else{
+    // Reencode to GIF, reading from and writing to folder_path
+    snprintf(ffmpeg, sizeof(ffmpeg),
+        "ffmpeg -i \"%s\\%s.mp4\" -vf \"fps=20,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" \"%s\\gif_%s.gif\"",
+        folder_path, temp_name, folder_path, buffer);
+    system(ffmpeg);
+
+    printf("Download complete\n");
+    printf("Deleting temporary file\n");
+
+    char del_cmd[256];
+    snprintf(del_cmd, sizeof(del_cmd), "del \"%s\\%s.mp4\"", folder_path, temp_name);
+    system(del_cmd);
+
+    printf("Temporary file deleted, Exiting program\n");
+}else{
         do{
             printf("Invalid format. Try again.\n");
             printf("1. mp4\n");
             printf("2. GIF\n");
             scanf("%d", &format);
-    
         } while (format != 1 && format != 2);
-
     }
     return 0;
 }
