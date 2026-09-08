6218b16ae6786611ee8ceabf48888f34bbe98b1f
xMoOnManFr <149280282+xMoOnManFr@users.noreply.github.com>
2026-09-08 20:29:33 +0200

    Restructuring
    Restructure of the code, not everything is held in a single function anymore

--- media downloader.c
+++ media downloader.c
@@ -3,15 +3,39 @@
 #include<stdlib.h>
 #include<time.h>
 
+char url[128];
+char *folder_path = "\%userprofile%\\Downloads";
+char *temp_name = "temp_vid";
+char yt_dlp_container[192];
+char ffmpeg[256];
+char *buffer;
+
+int generate_time(void){
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    buffer = malloc(32);
+
+    // %d = day, %m = month, %y = 2-digit year, %S = seconds, %M = minutes, %H = hours
+    strftime(buffer, sizeof(buffer), "%d_%m_%y_%H_%M_%S",t);
+}
+
+int yt_dlp(void){
+    snprintf(yt_dlp_container, sizeof(yt_dlp_container), "yt-dlp \"%s\" -o \"%s\\vid_\%s.mp4\"", url, folder_path, buffer);
+    system(yt_dlp_container);
+}
+
 int main(void){
+
     printf("Welcome to the Media Downloader!\n\n");
-    char url[256];
+    //int occurations = 0;
 
     //URL checking logic
     do {
+        memset(url,0,sizeof(url)); // Clear the url buffer
         printf("Enter a URL: ");
-        scanf("%128s", url);
+        scanf("%s", url);
     
+        //TODO: check for number of occurations of "https://" in the url, if more than 1, print error and ask for input again
         // Check condition: if INVALID, loop repeats
         if (strstr(url, "https://") != url) {
             printf("Invalid format. Try again.\n");
@@ -24,43 +48,28 @@ int main(void){
     printf("1. MP4\n");
     printf("2. GIF\n");
 
-    int format; 
+    int format[8]; 
     scanf("%d", &format);
 
-    //Vars needed for downloading and encoding magic
-    char yt_dlp[192];
-    char ffmpeg[256];
-    char *temp_name = "temp_vid";
-
-    //Logic to get different file names, also serves as a timestamp to sort files
-    time_t now = time(NULL);
-    struct tm *t = localtime(&now);
-    char buffer[80];
-
     //TODO: Write the file to the selected folder (If not bored create the folder/Verify it exist but will need extra imports)
-    //Change this path to save to another folder/If anyone uses this
     char *folder_path = "\%userprofile%\\Downloads";
-    
-    // %d = day, %m = month, %y = 2-digit year, %S = seconds, %M = minutes, %H = hours
-    strftime(buffer, sizeof(buffer), "%d_%m_%y_%H_%M_%S",t);
+
     
     //Logic to download as an MP4 
-    if(format == 1){
-        printf("You selected to download in .MP4\n");
+    if(*format == 1){
+        printf("You selected to download in .mp4\n");
         printf("The download will now start\n");
 
         //Download the video
-        snprintf(yt_dlp, sizeof(yt_dlp), "yt-dlp \"%s\" -o \"%s\\video_%s.mp4\"", url, folder_path, buffer);
-        system(yt_dlp);
+        yt_dlp();
     
     //Logic to download as an mp4 and reencode in GIF with ffmpeg
-    }else if(format == 2){
-    printf("You selected to download in .GIF\n");
+    }else if(*format == 2){
+    printf("You selected to download in .gif\n");
     printf("The download will now start\n");
 
-    snprintf(yt_dlp, sizeof(yt_dlp), "yt-dlp \"%s\" -o \"%s\\%s.mp4\"", url, folder_path, temp_name);
-    system(yt_dlp);
-
+    int yt_dlp();
+    
     // Reencode to GIF, reading from and writing to folder_path
     snprintf(ffmpeg, sizeof(ffmpeg),
         "ffmpeg -i \"%s\\%s.mp4\" -vf \"fps=20,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" \"%s\\gif_%s.gif\"",
@@ -71,18 +80,19 @@ int main(void){
     printf("Deleting temporary file\n");
 
     //Deleting the temp file needed for the reencoding
-    char del_cmd[256];
+    char del_cmd[64];
     snprintf(del_cmd, sizeof(del_cmd), "del \"%s\\%s.mp4\"", folder_path, temp_name);
     system(del_cmd);
 
     printf("Temporary file deleted, Exiting program\n");
+
 }else{
         do{
             printf("Invalid format. Try again.\n");
             printf("1. MP4\n");
             printf("2. GIF\n");
             scanf("%d", &format);
-        } while (format != 1 && format != 2);
+        } while (*format != 1 && *format != 2);
     }
     return 0;
 }
