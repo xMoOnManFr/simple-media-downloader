c42b4aa7ac77cf5d7f227dc72cc4b000b9e81984
xMoOnManFr <149280282+xMoOnManFr@users.noreply.github.com>
2026-09-08 20:33:47 +0200

    Fixed most all of the errors caused by my shit code, it now clears the -format- memory correctly if user input a char instead of integer which stops it looping back

--- media downloader.c
+++ media downloader.c
@@ -5,7 +5,6 @@
 
 char url[128];
 char *folder_path = "\%userprofile%\\Downloads";
-char *temp_name = "temp_vid";
 char yt_dlp_container[192];
 char ffmpeg[256];
 char *buffer;
@@ -14,12 +13,14 @@ int generate_time(void){
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
     buffer = malloc(32);
+    if (!buffer) return -1; //Vérification de malloc
 
-    // %d = day, %m = month, %y = 2-digit year, %S = seconds, %M = minutes, %H = hours
-    strftime(buffer, sizeof(buffer), "%d_%m_%y_%H_%M_%S",t);
+    strftime(buffer, 32, "%d_%m_%y_%H_%M_%S", t);
+    return 0;
 }
 
 int yt_dlp(void){
+    generate_time();
     snprintf(yt_dlp_container, sizeof(yt_dlp_container), "yt-dlp \"%s\" -o \"%s\\vid_\%s.mp4\"", url, folder_path, buffer);
     system(yt_dlp_container);
 }
@@ -38,25 +39,35 @@ int main(void){
         //TODO: check for number of occurations of "https://" in the url, if more than 1, print error and ask for input again
         // Check condition: if INVALID, loop repeats
         if (strstr(url, "https://") != url) {
-            printf("Invalid format. Try again.\n");
+            printf("Invalid url format. Try again.\n");
         }
     } while (strstr(url, "https://") != url);
 
     printf("URL successfully registered\n");
 
-    printf("In which format do you wish to save your file ?\n");
+    int result;
+    int format; 
+
+    do {
     printf("1. MP4\n");
     printf("2. GIF\n");
+    result = scanf("%d", &format);
 
-    int format[8]; 
-    scanf("%d", &format);
+    if (result != 1) {
+        // scanf a échoué : il y a un caractère invalide dans le buffer
+        printf("Invalid entry, the input must not be a number .\n");
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF); // vider le buffer
+        format = 0; // valeur qui ne matchera ni 1 ni 2, pour reboucler
+    }
+} while (format != 1 && format != 2);
 
     //TODO: Write the file to the selected folder (If not bored create the folder/Verify it exist but will need extra imports)
     char *folder_path = "\%userprofile%\\Downloads";
 
     
     //Logic to download as an MP4 
-    if(*format == 1){
+    if(format == 1){
         printf("You selected to download in .mp4\n");
         printf("The download will now start\n");
 
@@ -64,35 +75,29 @@ int main(void){
         yt_dlp();
     
     //Logic to download as an mp4 and reencode in GIF with ffmpeg
-    }else if(*format == 2){
+    }else if(format == 2){
     printf("You selected to download in .gif\n");
     printf("The download will now start\n");
 
-    int yt_dlp();
+    yt_dlp();
     
     // Reencode to GIF, reading from and writing to folder_path
     snprintf(ffmpeg, sizeof(ffmpeg),
-        "ffmpeg -i \"%s\\%s.mp4\" -vf \"fps=20,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" \"%s\\gif_%s.gif\"",
-        folder_path, temp_name, folder_path, buffer);
+    "ffmpeg -i \"%s\\vid_%s.mp4\" ""-vf \"fps=20,scale=480:-1:flags=lanczos,split[s0][s1];""[s0]palettegen[p];[s1][p]paletteuse\" ""\"%s\\gif_%s.gif\""
+    ,folder_path,buffer,folder_path,buffer);
     system(ffmpeg);
 
     printf("Download complete\n");
     printf("Deleting temporary file\n");
 
     //Deleting the temp file needed for the reencoding
-    char del_cmd[64];
-    snprintf(del_cmd, sizeof(del_cmd), "del \"%s\\%s.mp4\"", folder_path, temp_name);
+    //TODO: Add a check to verify the file is actually deleted before proceeding
+    char del_cmd;
+    snprintf(del_cmd, sizeof(del_cmd), "del \"%s\\vid_%s.mp4\"", folder_path, buffer);
     system(del_cmd);
 
     printf("Temporary file deleted, Exiting program\n");
 
-}else{
-        do{
-            printf("Invalid format. Try again.\n");
-            printf("1. MP4\n");
-            printf("2. GIF\n");
-            scanf("%d", &format);
-        } while (*format != 1 && *format != 2);
-    }
+}
     return 0;
 }
