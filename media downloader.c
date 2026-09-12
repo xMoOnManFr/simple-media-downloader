b940aa1d70daa0a7a10f0f5866ba431a41007e4b
xMoOnManFr <149280282+xMoOnManFr@users.noreply.github.com>
2026-09-12 20:56:54 +0200

    Started to add support for youtube

--- media downloader.c
+++ media downloader.c
@@ -1,111 +1,113 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 char url[192];
-char *folder_path = "\%userprofile%\\Downloads";
-char yt_dlp_container[192];
-char ffmpeg[256];
-char *buffer;
+char *folder_path = "%USERPROFILE%\\Downloads";  // FIX: removed stray backslash before %
+char yt_dlp_container[512];
+char ffmpeg_cmd[512];
 char video_name[128];
+char browser_name[32] = "";  // FIX: stores selected browser, empty = no cookies
 
-int generate_time(void){
+int generate_time(void) {
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
-    buffer = malloc(32);
-    if (!buffer) return -1;
-    strftime(buffer, 32, "%d_%m_%y_%H_%M_%S", t);
+    char timebuf[32];  // FIX: local buffer instead of malloc
+    strftime(timebuf, sizeof(timebuf), "%d_%m_%y_%H_%M_%S", t);
 
-    snprintf(video_name, sizeof(video_name),"vid_\%s\"",buffer);
-   return 0;
+    snprintf(video_name, sizeof(video_name), "vid_%s", timebuf);
+    return 0;
 }
 
-int yt_dlp(void){
-    //calls the function to actually write in memory
+int yt_dlp(void) {
     generate_time();
 
-    //builds the command
-    snprintf(yt_dlp_container, sizeof(yt_dlp_container), "yt-dlp \"%s\" -o \"%s\\%s.mp4\"", url, folder_path, video_name);
+    // Build base command
+    int len = snprintf(yt_dlp_container, sizeof(yt_dlp_container),
+                       "yt-dlp \"%s\" -o \"%s\\%s.mp4\"", url, folder_path, video_name);
 
-    //executes the command
+    // FIX: append cookies flag if a browser was selected
+    if (browser_name[0] != '\0') {
+        len += snprintf(yt_dlp_container + len, sizeof(yt_dlp_container) - len,
+                        " --cookies-from-browser %s", browser_name);
+    }
+
     system(yt_dlp_container);
+    return 0;
 }
 
-int main(void){
-
+int main(void) {
     printf("Welcome to the Media Downloader!\n\n");
-    //int occurations = 0;
 
-    //URL checking logic
     do {
-        memset(url,0,sizeof(url)); // Clear the url buffer
+        memset(url, 0, sizeof(url));
         printf("Enter a URL: ");
-        scanf("%s", url);
-    
-        //TODO: check for number of occurations of "https://" in the url, if more than 1, print error and ask for input again
-        // Check condition: if INVALID, loop repeats
+        scanf("%191s", url);
+
         if (strstr(url, "https://") != url) {
             printf("Invalid url format. Try again.\n");
         }
     } while (strstr(url, "https://") != url);
 
+    int browser = 0;
+    if (strstr(url, "youtube.com") != NULL) {  // FIX: != NULL is clearer than != url
+        printf("It appears you're trying to download a youtube video.\n");
+        printf("However youtube blocks video downloading if it doesn't come from a logged in user.\n");
+        printf("Please select a supported browser in which you are logged in:\n");
+        printf("1. Chrome\n2. Chromium\n3. Brave\n4. Firefox\n5. Opera\n6. Edge\n7. Whale\n");
+
+        // FIX: %d not %t
+        if (scanf("%d", &browser) == 1) {
+            switch (browser) {
+                case 1:  strcpy(browser_name, "chrome");    break;
+                case 2:  strcpy(browser_name, "chromium");  break;
+                case 3:  strcpy(browser_name, "brave");     break;
+                case 4:  strcpy(browser_name, "firefox");   break;
+                case 5:  strcpy(browser_name, "opera");     break;
+                case 6:  strcpy(browser_name, "edge");      break;
+                case 7:  strcpy(browser_name, "whale");     break;
+                default: printf("Invalid selection, no cookies will be used.\n");
+            }
+        }
+    }
+
     printf("URL successfully registered\n");
 
-    int result;
-    int format; 
-
+    int format;
     do {
-    printf("1. MP4\n");
-    printf("2. GIF\n");
-    result = scanf("%d", &format);
+        printf("1. MP4\n2. GIF\n");
+        int result = scanf("%d", &format);
+        if (result != 1) {
+            printf("Invalid entry, input must be a number.\n");
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+            format = 0;
+        }
+    } while (format != 1 && format != 2);
 
-    //Checks to catch a char in the buffer
-    if (result != 1) {
-        printf("Invalid entry, the input must not be a number .\n");
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF);
-        format = 0;
-    }
-} while (format != 1 && format != 2);
-
-    //TODO: Write the file to the selected folder (If not bored create the folder/Verify it exist but will need extra imports)
-    char *folder_path = "\%userprofile%\\Downloads";
-
-    
-    //Logic to download as an MP4 
-    if(format == 1){
+    if (format == 1) {
         printf("You selected to download in .mp4\n");
         printf("The download will now start\n");
-
-        //Download the video
         yt_dlp();
-    
-    //Logic to download as an mp4 and reencode in GIF with ffmpeg
-    }else if(format == 2){
-    printf("You selected to download in .gif\n");
-    printf("The download will now start\n");
+    } else if (format == 2) {
+        printf("You selected to download in .gif\n");
+        printf("The download will now start\n");
+        yt_dlp();
 
-    yt_dlp();
-    
-    // Reencode to GIF, reading from and writing to folder_path
-    snprintf(ffmpeg, sizeof(ffmpeg),
-    "ffmpeg -i \"%s\\%s.mp4\" ""-vf \"fps=20,scale=480:-1:flags=lanczos,split[s0][s1];""[s0]palettegen[p];[s1][p]paletteuse\" ""\"%s\\gif_%s.gif\""
-    ,folder_path,video_name,folder_path,video_name);
-    system(ffmpeg);
+        snprintf(ffmpeg_cmd, sizeof(ffmpeg_cmd),
+                 "ffmpeg -i \"%s\\%s.mp4\" -vf \"fps=20,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" \"%s\\gif_%s.gif\"",
+                 folder_path, video_name, folder_path, video_name);
+        system(ffmpeg_cmd);
 
-    printf("Download complete\n");
-    printf("Deleting temporary file\n");
+        printf("Download complete\n");
+        printf("Deleting temporary file\n");
 
-    //Deleting the temp file needed for the reencoding
-    //TODO: Add a check to verify the file is actually deleted before proceeding
-    //FILE *file = fopen(video_name, "r");
-    char del_cmd[64];
-    snprintf(del_cmd, sizeof(del_cmd), "del \"%s\\%s.mp4\"", folder_path, video_name);
-    system(del_cmd);
+        char del_cmd[256];
+        snprintf(del_cmd, sizeof(del_cmd), "del \"%s\\%s.mp4\"", folder_path, video_name);
+        system(del_cmd);
 
-    printf("Temporary file deleted, Exiting program\n");
-
-}
+        printf("Temporary file deleted, Exiting program\n");
+    }
     return 0;
-}
+}   
